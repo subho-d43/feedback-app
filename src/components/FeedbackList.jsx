d910cfb308203de694435515088dea6793cdf53c
Subhadeep <subhadeep.chakraborty2@in.ey.com>
2022-07-26 11:41:02 +0530

    changed package commands

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,20 +1,24 @@
 import { motion, AnimatePresence } from 'framer-motion'
+import { useContext } from 'react'
 import FeedbackItem from './FeedbackItem'
-import { feedback } from '../data/FeedbackData'
+import Spinner from './shared/Spinner'
+import FeedbackContext from '../context/FeedbackContext'
 
 // NOTE: added layout prop for nicer animation
 // https://www.framer.com/docs/animation/#layout-animations
 
 function FeedbackList() {
-  //const { feedback, isLoading } = useContext(FeedbackContext)
+  const { feedback, isLoading } = useContext(FeedbackContext)
 
-  // if (!isLoading && (!feedback || feedback.length === 0)) {
-  //   return <p>No Feedback Yet</p>
-  // }
+  if (!isLoading && (!feedback || feedback.length === 0)) {
+    return <p>No Feedback Yet</p>
+  }
 
   console.log(feedback);
 
-  return (
+  return isLoading ? (
+    <Spinner />
+  ) : (
     <div className='feedback-list'>
       <AnimatePresence>
         {feedback.map((item) => (
